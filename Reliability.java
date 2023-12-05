cefb2f37bb452092c9a9dcfddb40b28705ec3ddd
José Manuel de Torres Domínguez <pipornet@gmail.com>
2023-12-05 17:48:11 +0100

    Restablecido Reliability.java El anterior commit eliminaba por alguna razón el Reliability nuevo. Se ha vuelto a subir el archivo para corregir el error.

---
+++ Reliability.java
@@ -0,0 +1,21 @@
+/**
+ * Enumeration class reliable
+ * 
+ * @author Jose, Manuel & David
+ * @version 2023.11.27
+ */
+public enum Reliability
+{
+    HIGH(10),
+    LOW(5);
+
+    private final int Value;
+
+    Reliability(int Value){
+        this.Value = Value;
+    }
+
+    public int getValor(){
+        return(this.Value);
+    }
+}
