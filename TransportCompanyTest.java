70bc5936d43f429a7bb9dec3171d67e7b1084e9c
David <dmiguelp@alumnos.unex.es>
2023-12-06 22:32:07 +0100

    TransporCompanyTest

--- TransportCompanyTest.java
+++ TransportCompanyTest.java
@@ -13,6 +13,12 @@ public class TransportCompanyTest
 {
     TransportCompany company;
     TransportCompany company2;
+    
+    Passenger p1;
+    Passenger p2;
+    
+    Taxi taxi1;
+    Taxi taxi2;
     /**
      * Default constructor for test class TransportCompanyTest
      */
@@ -27,7 +33,22 @@ public class TransportCompanyTest
     public void setUp()
     {
         company = new TransportCompany("Compañía Taxis Cáceres");
-        company2 = new TransportCompany("ALSA Taxis");        
+        company2 = new TransportCompany("ALSA Taxis"); 
+        Location pickup1, pickup2, destination1, destination2;
+        int arrivalTime, creditCard;
+        Reliability reliable;
+        
+        pickup1 = new Location(0,0);
+        pickup2 = new Location(6,4);
+        destination1 = new Location(3,2);
+        destination2 = new Location(4,1);
+        
+        Passenger p1 = new PassengerVip(pickup1, destination1, "Javier", 1, 35000, Reliability.HIGH);
+        Passenger p2 = new PassengerNoVip(pickup2, destination2, "Juan", 1, 13000, Reliability.LOW);
+        
+        Taxi taxi1 = new TaxiShuttle(company, new Location(0, 0),"T1", FuelComsumption.MEDIUM, 4);
+        Taxi taxi2 = new TaxiExclusive(company2, new Location(0, 0),"T2",
+                    FuelComsumption.MEDIUM, 7000);
     }
     
     /**
@@ -46,13 +67,37 @@ public class TransportCompanyTest
     @Test
     public void testRequestPickup()
     {
-        Location pickup, destination;
-        String name;
-        int arrivalTime, creditCard;
-        Reliability reliable;
-        
-        pickup
-        Passenger p1 = new PassengerVip(pickup, destination, name, arrivalTime, creditCard,reliable);
+        assertEquals(false, company.requestPickup(p2));
+        assertEquals(false, company.requestPickup(p1));
+        assertEquals(true, company.requestPickup(p1));
     }
     
+    /**
+     * Test of the arrivedAtPickup() method
+     */    
+    @Test
+    public void testArrivedAtPickup()
+    {
+        
+        assertEquals(false, company.requestPickup(p2));
+        assertEquals(false, company.requestPickup(p1));
+        assertEquals(true, company.requestPickup(p1));
+    }
+    
+    /**
+     * Test of the arrivedAtDestination() method
+     */    
+    @Test
+    public void testArrivedAtDestination()
+    {
+        taxi1.pickup(p1);
+        for(int i = 1; i <= taxi1.distanceToTheTargetLocation(); i++){
+            taxi1.act();
+        }
+        assertEquals(p1.getPickup(), taxi1.getTargetLocation());
+        
+        assertEquals(false, company.requestPickup(p2));
+        assertEquals(false, company.requestPickup(p1));
+        assertEquals(true, company.requestPickup(p1));
+    }
 }
